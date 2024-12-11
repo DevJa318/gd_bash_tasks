8091fadcb1dfb87eb9d3ac96b1a0e0734f1cec89
DevJa318 <rkipek@gmail.com>
2024-12-11 09:48:32 +0100

    Remove unnecessary comments

--- FizzBuzz.sh
+++ FizzBuzz.sh
@@ -4,9 +4,6 @@
 # If the number is a multiple of 5, you need to print "Buzz" instead of that number.
 # If the number is a multiple of both 3 and 5, you need to print "FizzBuzz" instead of that number.
 
-# Shellcheck podkreśla mi $i%3 i $i%5, czy powinnam to jakoś zmienić?
-# -eq treats this as an arithmetic expression. Use = to compare as string (or expand explicitly with $((expr)))
-
 for i in {1..100}
 do
 if [[ $((i%3)) -eq 0 ]] && [[ $((i%5)) -eq 0 ]]; then
