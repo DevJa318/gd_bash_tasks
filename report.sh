e164e5ce4da0baf0c97eca78fcd6a21310f9fec9
DevJa318 <rkipek@gmail.com>
2024-12-11 10:04:01 +0100

    Add description

--- report.sh
+++ report.sh
@@ -11,16 +11,24 @@
 # number and frequency of CPU cores
 
 
-
+echo "# CURRENT DATE AND TIME"
 echo "$(date)"
+echo "# CURRENT USER"
 echo "$(whoami)"
+echo "# INTERNAL IP ADDRESS"
 echo "$(hostname -I)"
+echo "# HOSTNAME"
 echo "$(hostname)"
+echo "# EXTERNAL IP ADDRESS"
 echo "$(curl -s ifconfig.me)"
+echo "# NAME AND VERSION OF LINUX DISTRIBUTION"
 echo "$(grep -E '^(VERSION|NAME)=' /etc/os-release)"
+echo "# SYSTEM UPTIME"
 echo "$(uptime)"
+echo "# USED AND FREE SPACE IN / IN GB"
 echo "$(df -h /)"
+echo "# TOTAL AND FREE RAM"
 echo "$(free)"
-echo "$(lscpu | grep  ^CPU\( "
-#nie wiem jak sprawdzić CPU cores at all
+echo "# NUMBER AND FREQUENCY OF CPU CORES"
+echo "$(lscpu | grep  ^CPU\( )"
 echo "$(cat /proc/cpuinfo | grep MHz)"
